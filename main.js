bc150540960ac329cc6ec8222e9426d00b3ec36e
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-07-03 11:22:08 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+    // Open in new window
+        $('.click').each(function() {
+        var link = $(this).html();
+        $(this).contents().wrap('<a href="google.com"></a>');
+        });
+});
+
+// Make Card One Clickable
+function projectOne() {
+    window.open("http://www.baddebtsbuyer.com");
+}
+
+// Make Card Two Clickable
+function projectTwo() {
+    window.open("http://www.firstlandingcaremanagement.com");
+}
+
+// Make Card Three Clickable
+function projectThree() {
+    window.open("http://www.ismallcaps.com");
+}
