ad3dcadb7a63b95b5a1aa01620dec0c028f4543e
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-07-04 17:23:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,4 +26,20 @@ function projectFour(){
 window.open("https://qmassage.vercel.app/");
 }
 
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!  Someone Will Be in Touch");
+    })
+  });
+});
+
     
